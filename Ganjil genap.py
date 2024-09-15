bfa078921b1b2814b475055af7fe24ae7093d6eb
MANAN HIDAYATULLAH <133738124+Bos9091@users.noreply.github.com>
2024-09-15 17:53:38 +0800

    Create Ganjil genap.py

---
+++ Ganjil genap.py
@@ -0,0 +1,6 @@
+Mulai
+Input bilangan (misalnya, n).
+Jika n dibagi 2 sisa bagiannya adalah 0, maka:Bilangan tersebut adalah genap.
+Jika tidak, maka:Bilangan tersebut adalah ganjil.
+Output hasil dari langkah 3 dan 4.
+Selesai
