0ed2b3f8df12144dff7adf2954e2a4587d998d88
MANAN HIDAYATULLAH <133738124+Bos9091@users.noreply.github.com>
2024-09-15 17:50:47 +0800

    Create Luas lingkaran.py

---
+++ Luas lingkaran.py
@@ -0,0 +1,5 @@
+Mulai
+Input jari-jari lingkaran (misalnya, r).
+Hitung luas lingkaran menggunakan rumus ( \text{Luas} = \pi \times r^2 ), di mana ( \pi ) adalah konstanta sekitar 3.14159.
+Output nilai dari luas lingkaran.
+Selesai
